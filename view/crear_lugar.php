e469e099630cb9204a40adeac6531e946b89808e
Dani <100007160.joan23@fje.edu>
2021-12-20 22:09:15 +0100

    final all

--- view/crear_lugar.php
+++ view/crear_lugar.php
@@ -9,7 +9,7 @@ ob_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
-    <title>Inicio</title>
+    <title>CREAR LUGARES</title>
     <link rel="shortcut icon" href="../img/Aiga_restaurant_inv.svg.png" type="image/x-icon">
     <script src="../js/code.js"></script>
 </head>
